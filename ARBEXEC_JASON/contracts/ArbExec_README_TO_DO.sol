e76802e909601bbd7d4da878dfcda73d0d8de264
JJMcCaffrey <JasonMcCaffrey@Outlook.com>
2025-12-30 06:54:50 +0000

    Delete ARBEXEC_JASON directory

--- ARBEXEC_JASON/contracts/ArbExec_README_TO_DO.sol
+++
@@ -1,6 +0,0 @@
-// GOTO THE EMAIL SENT ON 25 DECEMBER 2025 @ 09:22
-// DOWNLOAD THE ArbExec.sol FILE AND INSERT IN THIS LOCATION
-// ArbOptimizer.sol is in the process of being superseded by ArbOptV2.sol. This adds more addresses and will be deployed with off-chain gas optimization.
-// Updating to monitor and check for front running and sandwich attacks.
-// =========DO NOT FORGET TO RENAME THE FOLDER /dot_git/ to /.git/
-// THEN INITILIZE THE REPOSITORY AFTER CLONING...
